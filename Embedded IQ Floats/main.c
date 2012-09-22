3e8bc5bcac4e0411f10ba3bac3f3fe552600b21c
JavierHz <hernandezb.javier@gmail.com>
2012-09-22 09:00:50 -0500

    Initial Commit

---
+++ Embedded IQ Floats/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Embedded IQ Floats
+//
+//  Created by Arturo Javier Hernández Barrón on 9/22/12.
+//  Copyright (c) 2012 Arturo Javier Hernández Barrón. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
